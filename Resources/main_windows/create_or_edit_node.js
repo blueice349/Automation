d454cc421f2a0ee9dd401b5562db290adcabed52
Vinay <gagantiwari@mac6s-Mac-mini.local>
2012-06-18 18:36:40 +0530

    Resolved issue #718

--- Resources/main_windows/create_or_edit_node.js
+++ Resources/main_windows/create_or_edit_node.js
@@ -59,6 +59,7 @@ create_or_edit_node.getWindow = function() {
 			fullscreen : false,
 			backgroundColor: '#111'
 		});
+		
 		//Sets only portrait mode
 		win.orientationModes = [Titanium.UI.PORTRAIT];
 
@@ -5733,18 +5734,34 @@ create_or_edit_node.loadUI = function() {
 	}
 
 	win.addEventListener('android:back', function() {
-		if(win.mode == 1) {
-			Ti.UI.createNotification({
-				message : win.title + ' update was cancelled !',
-				duration : Ti.UI.NOTIFICATION_DURATION_LONG
-			}).show();
-		} else {
-			Ti.UI.createNotification({
-				message : win.title + ' creation was cancelled !',
-				duration : Ti.UI.NOTIFICATION_DURATION_LONG
-			}).show();
-		}
-		win.close();
+		
+		
+		var dialog = Ti.UI.createAlertDialog({
+			cancel : 1,
+			buttonNames : ['Yes', 'No'],
+			message : 'Are you sure you want to cancel and go back?',
+			title : 'Cancel'
+		});
+
+		
+		dialog.addEventListener('click', function(e) {
+			if(e.index == 0) {
+				if(win.mode == 1) {
+					Ti.UI.createNotification({
+						message : win.title + ' update was cancelled !',
+						duration : Ti.UI.NOTIFICATION_DURATION_LONG
+					}).show();
+				} else {
+					Ti.UI.createNotification({
+						message : win.title + ' creation was cancelled !',
+						duration : Ti.UI.NOTIFICATION_DURATION_LONG
+					}).show();
+				}
+				win.close();
+			}
+		});
+
+		dialog.show();
 	});
 
 	toolActInd.hide();
