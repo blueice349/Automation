79bbe57888d4c6b6d8c978cd13d676a2258b1338
Joseandro Luiz <eng.joseandro@gmail.com>
2012-08-09 18:00:04 -0300

    Version change

--- Resources/app.js
+++ Resources/app.js
@@ -25,7 +25,7 @@ var win1 = Titanium.UI.createWindow({
 	backgroundColor: '#EEEEEE'
 });
 
-var OMADI_VERSION = "omadiDb1573";
+var OMADI_VERSION = "omadiDb1574";
 
 Titanium.App.Properties.setString("databaseVersion", OMADI_VERSION);
 var db = Ti.Database.install('/database/db_list.sqlite', Titanium.App.Properties.getString("databaseVersion")+"_list" );
