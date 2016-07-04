236e22935598d138c7295a3a8228a90672b9f258
Mike Meyer <mmeyer@omadi.com>
2016-07-04 16:06:26 -0600

    Style changes.

--- helpers/logging.js
+++ helpers/logging.js
@@ -1,14 +1,19 @@
 "use strict";
 
-exports.configure = function (driver) {
+exports.configure = function ( driver ) {
   // See whats going on
-  driver.on('status', function (info) {
-    console.log(info.cyan);
-  });
-  driver.on('command', function (meth, path, data) {
-    console.log(' > ' + meth.yellow, path.magenta, data || '');
-  });
-  driver.on('http', function (meth, path, data) {
-    console.log(' > ' + meth.magenta, path.cyan, (data || '').cyan);
-  });
+   driver.on( 'status', function ( info ) {
+    
+      console.log( info.cyan );
+  } );
+
+  driver.on( 'command', function ( meth, path, data ) {
+
+    console.log( ' > ' + meth.yellow, path.magenta, data || '' );
+  } );
+
+  driver.on( 'http', function ( meth, path, data ) {
+
+    console.log( ' > ' + meth.magenta, path.cyan, (data || '' ).cyan );
+  } );
 };
