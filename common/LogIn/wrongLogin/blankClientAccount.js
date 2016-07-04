ad1c9d475aed5f35f3d8bc74ada0714bfe27d47d
Mike Meyer <mmeyer@omadi.com>
2016-07-04 16:05:19 -0600

    # Created more login test # Rename badLogin to wrongLogin

---
+++ common/LogIn/wrongLogin/blankClientAccount.js
@@ -0,0 +1,114 @@
+'use strict';
+
+module.exports = function () {
+
+	require( 'colors' );
+	var alert    = require( '../../../helpers/alerts' );
+	var config   = require( '../../../helpers/Config' );
+	var elements = require( '../../../helpers/elements' );
+	var commons  = require( '../../../helpers/Commons' );
+	var login    = require( '../../../helpers/loginTable' );
+
+
+	var driver = config.driver;
+
+	describe( 'Start try login with blank ClientAccount process using blankClientAccount.js'.green, function() {
+
+		it( 'Should make sure clientAccount is blank'.green, function () {
+
+			return driver
+			.waitForElementByName( elements.loginScreen.clientAccount, 200000 )
+			.then( function ( el ) {
+
+					return commons.sendKeys(el, '' );
+			} )
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'Should enter a good userName'.green, function () {
+
+			return driver
+			.elementByName( elements.loginScreen.userName )
+			.then( function ( el ) {
+
+				if ( commons.isIOS() ) {
+					return commons.sendKeys( el, login.driverLogins.driver1.username );
+
+				} else {
+					return commons.sendKeys( el, login.driverLogins.driver2.username );
+				}
+			} )
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'Should enter a good password.'.green, function () {
+
+			return driver
+			.elementByName( elements.loginScreen.password )
+			.then( function ( el ) {
+				
+				if ( commons.isIOS() ) {
+					return commons.sendKeys( el, login.driverLogins.driver1.password );
+
+				} else {
+					return commons.sendKeys( el, login.driverLogins.driver2.password );
+				}
+			} )
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'Should acceptTerms.'.green, function () {
+
+			return driver
+			.elementByName( elements.loginScreen.acceptTerms )
+			.click()
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'Should click the loginButton.'.green, function () {
+
+			return driver
+			.elementByName( elements.loginScreen.loginButton )
+			.click()
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'should get alert for blank clientAccount'.green, function () {
+
+			return driver
+			.waitForElementByName( elements.alertButtons.ok, 120000 )
+			.then( function ( el ) {
+
+				return commons.alertText( alerts.loginLogoutAlerts.blankClientAccount );
+				el.click()
+				.sleep( 1000 )
+			} )
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'should set currentTest to "passed".'.green, function ( done ) {
+			
+			console.log( 'blankClientAccount test has Completed....'.green );
+			config.currentTest = 'passed';
+			done();
+		} );
+	});
+};
---
+++ common/LogIn/wrongLogin/blankPassword.js
@@ -0,0 +1,114 @@
+'use strict';
+
+module.exports = function () {
+
+	require( 'colors' );
+	var alert    = require( '../../../helpers/alerts' );
+	var config   = require( '../../../helpers/Config' );
+	var elements = require( '../../../helpers/elements' );
+	var commons  = require( '../../../helpers/Commons' );
+	var login    = require( '../../../helpers/loginTable' );
+
+
+	var driver = config.driver;
+
+	describe( 'Start try login with blank password process using blankPassword.js'.green, function() {
+
+		it( 'Should enter good clientAccount'.green, function () {
+
+			return driver
+			.waitForElementByName( elements.loginScreen.clientAccount, 200000 )
+			.then( function ( el ) {
+
+				if ( commons.isIOS() ) {
+					return commons.sendKeys( el, login.driverLogins.driver1.clientAccount );
+
+				} else {
+					return commons.sendKeys( el, login.driverLogins.driver2.clientAccount );
+				}
+			} )
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'Should enter good userName'.green, function () {
+
+			return driver
+			.elementByName( elements.loginScreen.userName )
+			.then( function ( el ) {
+
+				if ( commons.isIOS() ) {
+					return commons.sendKeys( el, login.driverLogins.driver1.username );
+
+				} else {
+					return commons.sendKeys( el, login.driverLogins.driver2.username );
+				}
+			} )
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'Should make sure password is blank.'.green, function () {
+
+			return driver
+			.elementByName( elements.loginScreen.password )
+			.then( function ( el ) {
+
+					return commons.sendKeys( el, '' );
+			} )
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'Should acceptTerms.'.green, function () {
+
+			return driver
+			.elementByName( elements.loginScreen.acceptTerms )
+			.click()
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'Should click the loginButton.'.green, function () {
+
+			return driver
+			.elementByName( elements.loginScreen.loginButton )
+			.click()
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'should get alert for blank password'.green, function () {
+
+			return driver
+			.waitForElementByName( elements.alertButtons.ok, 120000 )
+			.then( function ( el ) {
+
+				return commons.alertText( alerts.loginLogoutAlerts.blankPassword );
+				el.click()
+				.sleep( 1000 )
+			} )
+			.then( function () {
+
+				config.currentTest = 'passed';
+			} );
+		} );
+
+		it( 'should set currentTest to "passed".'.green, function ( done ) {
+			
+			console.log( 'blankPassword test has Completed....'.green );
+			config.currentTest = 'passed';
+			done();
+		} );
+	});
+};
